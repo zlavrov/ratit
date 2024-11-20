b36dbfb0e9abae394f65bda060ea2aca9ae3b4f0
zlavrov <zakhar.polosky@gmail.com>
2024-11-20 13:02:53 +0200

    change route

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -6,13 +6,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController extends AbstractController
+class PageController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
+        return $this->render('page/index.html.twig', [
             'controller_name' => 'HomeController',
+        ]);
+    }
+
+    #[Route('/about', name: 'app_about')]
+    public function about(): Response
+    {
+        return $this->render('page/index.html.twig', [
+            'controller_name' => 'AboutController',
         ]);
     }
 }
